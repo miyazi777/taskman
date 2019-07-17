adf566322a725b92a8d4b84b9c7e9228fbe64c32
miyazi777 <m71203@gmail.com>
2019-07-17 22:52:13 +0900

    first commit

---
+++ cmd/init.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the info command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "",
+	Long:  "",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		fmt.Println("init command.")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// infoCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// infoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/miyazi777/taskman
+
+go 1.12
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/miyazi777/git-desc v0.1.2 // indirect
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.4.0
+)
